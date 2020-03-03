d9e524cb6907b01aca0c99f44d9c34dfc5c9dd47
aficat <aficatrashid@gmail.com>
2020-03-03 17:10:08 +0800

    updated with material ui assets and components

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 const debug = process.env.NODE_ENV !== "production";
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const withSass = require("@zeit/next-sass");
+const webpack = require("webpack");
+const path = require("path");
 
-module.exports = {
+module.exports = withPlugins([[withSass], [withImages]], {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
-      // "/about": { page: "/about" },
+      "/about": { page: "/about" },
+      "/components": { page: "/components" },
+      "/login": { page: "/login" },
+      "/landing": { page: "/landing" },
+      "/profile": { page: "/profile" },
     }
   },
   //assetPrefix: '',
@@ -14,11 +23,11 @@ module.exports = {
     // console.log('webpack');
     // console.log(config.module.rules, dev);
     config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
+      if (rule.loader === 'babel-loader') {
         rule.options.cacheDirectory = false
       }
       return rule
     })
     return config
   }
-}
+})
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import MuiLink from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
-import Router from "next/router";
 
 function Copyright() {
   return (
@@ -20,25 +20,19 @@ function Copyright() {
   );
 }
 
-export default class Index extends Component {
-  // componentDidMount = () => {
-  //   Router.push("/components");
-  // };
-
-  render() {
-    return (
-      <Container maxWidth="sm">
-        <Box my={4}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Next.js example
+export default function About() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js example
         </Typography>
-          <Link href="/about" color="secondary">
-            Go to the about page
-        </Link>
-          <ProTip />
-          <Copyright />
-        </Box>
-      </Container>
-    );
-  }
+        <Button variant="contained" color="primary" component={Link} naked href="/">
+          Go to the main page
+        </Button>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
